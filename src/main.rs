9edf0284a1ff0a2e945a2eae9bc439ba25470f4a
codingmentalmodels <codingmentalmodels@gmail.com>
2023-02-16 14:27:16 -0500

    Get current directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod symbol;
 mod workspace;
 
+use std::{env::current_dir};
+use clap::{arg, Command, Arg, Subcommand};
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("Symbolic Manipulation Engine")
+        .version("0.0")
+        .author("Coding Mental Models <codingmentalmodels@gmail.com>")
+        .about("Symbolic Manipulation Engine for performing mathematical operations")
+        .propagate_version(true)
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initializes a new workspace")
+        ).get_matches();
+
+        match matches.subcommand() {
+            Some(("init", sub_matches)) => {
+                println!("Initializing new workspace in {}", current_dir().unwrap().display());
+            },
+            _ => {
+                println!("No subcommand was used");
+            }
+        }
+
+
 }
