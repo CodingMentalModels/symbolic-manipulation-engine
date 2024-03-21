b609c5393665c64258b9fd7e46cbffb52f1c3508
codingmentalmodels <codingmentalmodels@gmail.com>
2024-03-20 21:16:17 -0500

    Intermediate state thinking through how to import contexts

--- src/workspace/workspace.rs
+++ src/workspace/workspace.rs
@@ -1,8 +1,11 @@
 use std::collections::{HashMap, HashSet};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-use crate::symbol::{symbol_node::{SymbolNode, SymbolNodeAddress}, transformation::{Transformation, TransformationError}};
+use crate::symbol::{
+    symbol_node::{SymbolNode, SymbolNodeAddress},
+    transformation::{Transformation, TransformationError},
+};
 
 type StatementIndex = usize;
 type TransformationIndex = usize;
@@ -15,7 +18,6 @@ pub struct Workspace {
 }
 
 impl Workspace {
-
     pub fn new() -> Workspace {
         Self::default()
     }
@@ -29,7 +31,12 @@ impl Workspace {
         self.transformations.push(transformation);
     }
 
-    pub fn transform_all(&mut self, transformation_index: TransformationIndex, statement_index: StatementIndex, substitutions: HashMap<String, String>) -> Result<SymbolNode, WorkspaceError> {
+    pub fn transform_all(
+        &mut self,
+        transformation_index: TransformationIndex,
+        statement_index: StatementIndex,
+        substitutions: HashMap<String, String>,
+    ) -> Result<SymbolNode, WorkspaceError> {
         if self.transformation_index_is_invalid(transformation_index) {
             return Err(WorkspaceError::InvalidTransformationIndex);
         }
@@ -38,15 +45,26 @@ impl Workspace {
         }
         let transformation = self.transformations[transformation_index].clone();
         let statement = self.statements[statement_index].clone();
-        let (transformed_statement, transformed_addresses) = transformation.transform_all(statement, substitutions).map_err(|e| WorkspaceError::TransformationError(e))?;
+        let (transformed_statement, transformed_addresses) = transformation
+            .transform_all(statement, substitutions)
+            .map_err(|e| WorkspaceError::TransformationError(e))?;
 
         self.statements.push(transformed_statement.clone());
-        self.provenance.push(Provenance::Derived((statement_index, transformation_index, transformed_addresses)));
+        self.provenance.push(Provenance::Derived((
+            statement_index,
+            transformation_index,
+            transformed_addresses,
+        )));
 
         return Ok(transformed_statement);
     }
 
-    pub fn transform_at(&mut self, transformation_index: TransformationIndex, statement_index: StatementIndex, address: SymbolNodeAddress) -> Result<SymbolNode, WorkspaceError> {
+    pub fn transform_at(
+        &mut self,
+        transformation_index: TransformationIndex,
+        statement_index: StatementIndex,
+        address: SymbolNodeAddress,
+    ) -> Result<SymbolNode, WorkspaceError> {
         if self.transformation_index_is_invalid(transformation_index) {
             return Err(WorkspaceError::InvalidTransformationIndex);
         }
@@ -55,15 +73,24 @@ impl Workspace {
         }
         let transformation = self.transformations[transformation_index].clone();
         let statement = self.statements[statement_index].clone();
-        let transformed_statement = transformation.transform_at(statement, address.clone()).map_err(|_| WorkspaceError::InvalidTransformationAddress)?;
+        let transformed_statement = transformation
+            .transform_at(statement, address.clone())
+            .map_err(|_| WorkspaceError::InvalidTransformationAddress)?;
 
         self.statements.push(transformed_statement.clone());
-        self.provenance.push(Provenance::Derived((statement_index, transformation_index, vec![address])));
+        self.provenance.push(Provenance::Derived((
+            statement_index,
+            transformation_index,
+            vec![address],
+        )));
 
         return Ok(transformed_statement);
     }
 
-    pub fn get_provenance_lineage(&self, index: StatementIndex) -> Result<Vec<Provenance>, WorkspaceError> {
+    pub fn get_provenance_lineage(
+        &self,
+        index: StatementIndex,
+    ) -> Result<Vec<Provenance>, WorkspaceError> {
         let mut provenance = Vec::new();
         let mut current_index = index;
         loop {
@@ -90,20 +117,25 @@ impl Workspace {
         let transformation_header = "Transformations:".to_string();
         to_return.push(transformation_header);
 
-        self.transformations.iter().enumerate().for_each(|(index, transformation)| {
-            to_return.push(format!("{}: {}", index, transformation.to_string()));
-        });
+        self.transformations
+            .iter()
+            .enumerate()
+            .for_each(|(index, transformation)| {
+                to_return.push(format!("{}: {}", index, transformation.to_string()));
+            });
         to_return.push("".to_string());
 
         let statement_header = "Statements:".to_string();
         to_return.push(statement_header);
 
-        self.statements.iter().enumerate().for_each(|(index, statement)| {
-            to_return.push(format!("{}: {}", index, statement.to_string()));
-        });
+        self.statements
+            .iter()
+            .enumerate()
+            .for_each(|(index, statement)| {
+                to_return.push(format!("{}: {}", index, statement.to_string()));
+            });
 
         return to_return.join("\n");
-        
     }
 
     pub fn serialize(&self) -> String {
@@ -121,7 +153,6 @@ impl Workspace {
     fn transformation_index_is_invalid(&self, index: TransformationIndex) -> bool {
         index >= self.transformations.len()
     }
-
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -140,6 +171,10 @@ pub enum WorkspaceError {
 
 #[cfg(test)]
 mod test_workspace {
+
+    use std::unimplemented;
+
+    use crate::context::context::Context;
 
     use super::*;
 
@@ -165,31 +200,76 @@ mod test_workspace {
 
         let transformation = Transformation::new(
             SymbolNode::leaf_object("a".to_string()),
-            SymbolNode::leaf_object("b".to_string())
+            SymbolNode::leaf_object("b".to_string()),
         );
         workspace.add_transformation(transformation);
         assert_eq!(workspace.transformations.len(), 1);
 
         workspace.transform_all(0, 0, HashMap::new());
         assert_eq!(workspace.statements.len(), 2);
-        assert_eq!(workspace.statements, vec![SymbolNode::leaf_object("a".to_string()), SymbolNode::leaf_object("b".to_string())]);
+        assert_eq!(
+            workspace.statements,
+            vec![
+                SymbolNode::leaf_object("a".to_string()),
+                SymbolNode::leaf_object("b".to_string())
+            ]
+        );
 
         assert_eq!(workspace.get_provenance(0), Ok(Provenance::Hypothesis));
-        assert_eq!(workspace.get_provenance(1), Ok(Provenance::Derived((0, 0, vec![vec![]]))));
+        assert_eq!(
+            workspace.get_provenance(1),
+            Ok(Provenance::Derived((0, 0, vec![vec![]])))
+        );
 
-        assert_eq!(workspace.get_provenance_lineage(0), Ok(vec![Provenance::Hypothesis]));
-        assert_eq!(workspace.get_provenance_lineage(1), Ok(vec![Provenance::Derived((0, 0, vec![vec![]])), Provenance::Hypothesis]));
+        assert_eq!(
+            workspace.get_provenance_lineage(0),
+            Ok(vec![Provenance::Hypothesis])
+        );
+        assert_eq!(
+            workspace.get_provenance_lineage(1),
+            Ok(vec![
+                Provenance::Derived((0, 0, vec![vec![]])),
+                Provenance::Hypothesis
+            ])
+        );
 
         workspace.add_transformation(Transformation::new(
             SymbolNode::leaf_object("b".to_string()),
-            SymbolNode::leaf_object("=".to_string())
+            SymbolNode::leaf_object("=".to_string()),
         ));
 
         workspace.transform_at(1, 1, vec![]);
         assert_eq!(workspace.statements.len(), 3);
-        assert_eq!(workspace.statements, vec![SymbolNode::leaf_object("a".to_string()), SymbolNode::leaf_object("b".to_string()), SymbolNode::leaf_object("=".to_string())]);
+        assert_eq!(
+            workspace.statements,
+            vec![
+                SymbolNode::leaf_object("a".to_string()),
+                SymbolNode::leaf_object("b".to_string()),
+                SymbolNode::leaf_object("=".to_string())
+            ]
+        );
 
-        assert_eq!(workspace.get_provenance(2), Ok(Provenance::Derived((1, 1, vec![vec![]]))));
+        assert_eq!(
+            workspace.get_provenance(2),
+            Ok(Provenance::Derived((1, 1, vec![vec![]])))
+        );
+    }
 
+    #[test]
+    fn test_workspace_imports_context() {
+        let mut workspace = Workspace::new();
+
+        let context = Context::new();
+
+        workspace.import_context(context);
+
+        let ambiguous_context = Context::new();
+
+        workspace.import_context(context);
+
+        assert_eq!(workspace.transformations.len(),);
+
+        unimplemented!()
     }
 }
+
