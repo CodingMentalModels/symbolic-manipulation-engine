46f0728a2d4ba5480523a90ea6c0861dcbb38221
codingmentalmodels <codingmentalmodels@gmail.com>
2024-04-19 15:34:46 -0400

    Check for different number of children

--- src/symbol/symbol_node.rs
+++ src/symbol/symbol_node.rs
@@ -488,6 +488,14 @@ impl SymbolNode {
                 .collect();
             Ok((relabelling, substitutions))
         } else {
+            if self.get_n_children() != other.get_n_children() {
+                return Err(SymbolNodeError::DifferentNumberOfArguments(
+                    self.get_symbol().clone(),
+                    self.get_n_children(),
+                    other.get_symbol().clone(),
+                    other.get_n_children(),
+                ));
+            }
             self.get_children()
                 .iter()
                 .zip(other.get_children())
